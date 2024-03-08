5a06a976f47976314da05c2998a8b6ddf5ed1195
haemalgeumi <abk011@naver.com>
2024-03-08 18:31:20 +0900

    AWS S3및 이미지 업로드 기능

--- server.js
+++ server.js
@@ -6,14 +6,35 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt') 
 const MongoStore = require('connect-mongo')
+require('dotenv').config() 
+const { S3Client } = require('@aws-sdk/client-s3')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const s3 = new S3Client({
+  region : 'ap-northeast-2',
+  credentials : {
+      accessKeyId :  process.env.S3_KEY,
+      secretAccessKey : process.env.S3_SECRET
+  }
+})
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'yuniforum6',
+    key: function (요청, file, cb) {
+      cb(null, Date.now().toString()) //업로드시 파일명 변경가능
+    }
+  })
+})
 
 let db
-const url = 'mongodb+srv://admin:qwer1234@cluster0.pkaqcij.mongodb.net/?retryWrites=true&w=majority'
+const url =  process.env.DB_URL
 new MongoClient(url).connect().then((client)=>{
   console.log('DB연결성공')
   db = client.db('forum')
 
-  app.listen(8080, () => {
+  app.listen( process.env.PORT, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 }).catch((err)=>{
@@ -95,17 +116,21 @@ app.get('/detail/:id', async(요청, 응답)=>{
 })
 
 app.post('/add', (요청, 응답)=>{
- try{
-  if(요청.body.title === '' || 요청.body.content === ''){
-    응답.send('내용을 입력하시오')
-  } else{
-    db.collection('post').insertOne({title: 요청.body.title, content: 요청.body.content})
-    응답.redirect('/list')
-    console.log(요청.body);
-  }
- } catch{
-  응답.send('서버 에러났어요')
- }
+  upload.single("img1") (요청,응답,(err)=>{
+    if (err) return 응답.send('에러남')
+    try{
+      if(요청.body.title === '' || 요청.body.content === ''){
+        응답.send('내용을 입력하시오')
+      } else{
+        db.collection('post').insertOne({title: 요청.body.title, content: 요청.body.content, img : 요청.file.location})
+        응답.redirect('/list')
+        console.log(요청.body);
+      }
+     } catch{
+      응답.send('서버 에러났어요')
+     }
+  })
+ 
 
 })
 
